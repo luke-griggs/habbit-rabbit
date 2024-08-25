20e81b0dadd09103914c78e185a8eae808db47f8
Luke <lukegriggs100@gmail.com>
2024-08-25 18:53:49 -0500

    local activity adding working

--- main.go
+++ main.go
@@ -3,40 +3,73 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
+type Activity struct {
+	name string
+	timeframe string
+	time int
+}
+
 func main() {
-    // Define a handler function
-	
-	activities := map[string]string{}
+	// Define a handler function
+
+	activities := []Activity{}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
 			http.ServeFile(w, r, "./application")
 		}
-		
-    })
-
-    http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<p>the current time is: %s</p>", time.Now().Format("3:04"))
-    })
-	http.HandleFunc("/activities", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST"{
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
-
-		activityName := r.FormValue("activity")
-		activityHours := r.FormValue("hours")
-		activities[activityName] = activityHours
-
-		fmt.Fprintf(w, "<p> %s was added to your activities</p>", activityName)
 
 	})
 
-    // Start an HTTP server listening on port 8080
-    fmt.Println("Server is running on http://localhost:8080/")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        fmt.Println(err)
-    }
+	http.HandleFunc("/addActivity", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+
+		activityName := r.FormValue("activityName")
+		activityTimeFrame := r.FormValue("timeFrame")
+
+		activities = append(activities, Activity{activityName, activityTimeFrame, 0})
+
+		w.Write([]byte(""))
+
+	})
+
+
+	http.HandleFunc("/getActivityForm", func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Fprintf(w,
+			`
+			<form hx-target="this" hx-post="/addActivity">
+			<p>what's the name of the activity you'd like to track?</p>
+			<input type="text" name="activityName" placeholder="reading">
+			<p>on what timeframe would you like to track this activity</p>
+			<input type="radio" id="Daily" name="timeFrame" value="Daily">
+			<label for="Daily">Daily</label><br>
+			<input type="radio" id="Weekly" name="timeFrame" value="Weekly">
+			<label for="Weekly">Weekly</label><br>
+			<input type="radio" id="Monthly" name="timeFrame" value="Monthly">
+			<label for="Monthly">Monthly</label><br>
+			<button type="submit">create activity</button>
+			</form>
+		`)
+	})
+
+	http.HandleFunc("/displayActivities", func(w http.ResponseWriter, r *http.Request){
+		if len(activities) > 0 {
+			for _, activity := range activities {
+				fmt.Fprintf(w, `<p>Activity: %s, Hours spent: 0, time frame: %s</p>`, activity.name, activity.timeframe)
+			}
+		} else {
+			w.Write([]byte("<p>your activities will show up here when you add them</p>"))
+		}
+	})
+
+	// Start an HTTP server listening on port 8080
+	fmt.Println("Server is running on http://localhost:8080/")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
 }
